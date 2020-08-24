d9bdcace4a52124fc4509344f90e64e5a8054cb7
aslampangestu <aslampangestu@gmai.com>
2020-08-24 08:16:11 +0700

    init

---
+++ components/Head.js
@@ -0,0 +1,12 @@
+import HeadHTML from "next/head";
+
+const Head = () => {
+  return (
+    <HeadHTML>
+      <title>Aslam Pangestu</title>
+      <link rel="icon" href="/favicon.ico" />
+    </HeadHTML>
+  );
+};
+
+export default Head;
